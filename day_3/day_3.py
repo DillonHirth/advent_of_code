a166be8748891381b8eacb954aa10c65a75ee06e
dillon <dhirth.it@gmail.com>
2021-12-05 15:53:03 -0600

    day3_part2

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -32,10 +32,74 @@ gamma_binary = int(''.join([str(integer) for integer in gamma]))
 gamma_decimal = binary_to_decimal(gamma_binary)
 epsilon_binary = int(''.join(['1' if i == '0' else '0' for i in str(gamma_binary)]))
 epsilon_decimal = binary_to_decimal(epsilon_binary)
-print(gamma)
-print("gamma_binary: ", gamma_binary)
-print("gamma_decimal: ", gamma_decimal)
-print("epsilon_binary: ", epsilon_binary)
-print("epsilon_decimal: ", epsilon_decimal)
 power_consumption = gamma_decimal * epsilon_decimal
 print("power_consumption: ", power_consumption)
+
+# Part 2
+with open('input.txt') as input_file:
+    input_list = input_file.readlines()
+
+
+def oxy_gen_rating(sweet_list, pointer=0):
+    new_list = []
+    filter = bit_criteria_more(sweet_list, pointer)
+    for item in sweet_list:
+        if int(item[pointer]) == filter:
+            new_list.append(item.strip('\n'))
+    pointer += 1
+    if len(new_list) > 1:
+        return oxy_gen_rating(new_list, pointer)
+    else:
+        oxy_gen_binary = int(''.join([str(integer) for integer in new_list]))
+        return binary_to_decimal(oxy_gen_binary)
+
+
+def co2_scrub_rating(sweet_list, pointer=0):
+    new_list = []
+    filter = bit_criteria_fewer(sweet_list, pointer)
+    for item in sweet_list:
+        if int(item[pointer]) == filter:
+            new_list.append(item.strip('\n'))
+    pointer += 1
+    if len(new_list) > 1:
+        return co2_scrub_rating(new_list, pointer)
+    else:
+        co2_scrub_binary = int(''.join([str(integer) for integer in new_list]))
+        return binary_to_decimal(co2_scrub_binary)
+
+
+def bit_criteria_more(sweet_list, position=0):
+    # create empty list
+    current_item = [0] * len(sweet_list[0].strip('\n'))
+    count = 0
+    for list_item in sweet_list:
+        next_item = list(list_item.strip('\n'))
+        result = zip(current_item, next_item)
+        sum_list = [int(x) + int(y) for (x, y) in result]
+        current_item = sum_list
+        count += 1
+    if sum_list[position] >= count / 2:
+        return 1
+    else:
+        return 0
+
+
+def bit_criteria_fewer(sweet_list, position=0):
+    # create empty list
+    current_item = [0] * len(sweet_list[0].strip('\n'))
+    count = 0
+    for list_item in sweet_list:
+        next_item = list(list_item.strip('\n'))
+        result = zip(current_item, next_item)
+        sum_list = [int(x) + int(y) for (x, y) in result]
+        current_item = sum_list
+        count += 1
+    if sum_list[position] >= count / 2:
+        return 0
+    else:
+        return 1
+
+oxy_gen_rating(input_list)
+co2_scrub_rating(input_list)
+life_support_rating = oxy_gen_rating(input_list) * co2_scrub_rating(input_list)
+print("life_support_rating: ", life_support_rating)
