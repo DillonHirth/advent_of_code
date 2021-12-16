4342a7a0c4fff9fd04268bf616acb535422a60e1
dillon <dhirth.it@gmail.com>
2021-12-16 02:20:08 -0600

    day9_part1_progress_for_part2

---
+++ day_9/day_9.py
@@ -0,0 +1,142 @@
+# PART 1
+low_points = []
+def check_basin_crawler(point):
+    points = []
+    #print("point to check: ", point)
+    row = point[0]
+    col = point[1]
+    if row == 0:
+        next_row = row + 1
+        if input_list[next_row][col] != '9':
+            points.append((next_row, col))
+    elif row == len(input_list)-1:
+        prev_row = row - 1
+        if input_list[prev_row][col] != '9':
+            points.append((prev_row, col))
+    else:
+        prev_row = row - 1
+        next_row = row + 1
+        if input_list[prev_row][col] != '9':
+            points.append((prev_row, col))
+        if input_list[next_row][col] != '9':
+            points.append((next_row, col))
+
+    if col == 0:
+        next_col = col + 1
+        if input_list[row][next_col] != '9':
+            points.append((row, next_col))
+    elif col == len(input_list[row])-1:
+        prev_col = col - 1
+        if input_list[row][prev_col] != '9':
+            points.append((row, prev_col))
+    else:
+        prev_col = col - 1
+        next_col = col + 1
+        if input_list[row][prev_col] != '9':
+            points.append((row, prev_col))
+        if input_list[row][next_col] != '9':
+            points.append((row, next_col))
+    return points
+
+
+
+
+
+
+
+def check_basin(point):
+    basin_found = []
+    to_search = []
+    found_points = []
+    basin_found.append(point)
+    to_search.append(point)
+    search = True
+    while search == True:
+        for point in to_search:
+            found_points += check_basin_crawler(point)
+        completed_search = found_points + to_search
+        to_search = [x for x in found_points if x not in basin_found]
+        found_points = []
+        for item in completed_search:
+            if item not in basin_found:
+                basin_found.append(item)
+        if to_search:
+            #print("searching:", to_search)
+            pass
+        else:
+            #print("empty")
+            search = False
+    #print("completed_search: ", completed_search)
+    print("Basin_Found:", basin_found)
+
+
+def check_horizontal(row_col_tuple):
+    row = row_col_tuple[0]
+    col = row_col_tuple[1]
+    if col == 0:
+        next_col = col + 1
+        if input_list[row][next_col] > input_list[row][col]:
+            return True
+        return False
+    elif col == len(input_list[row])-1:
+        prev_col = col - 1
+        if input_list[row][prev_col] > input_list[row][col]:
+            return True
+        return False
+    else:
+        prev_col = col - 1
+        next_col = col + 1
+        if input_list[row][next_col] > input_list[row][col] and input_list[row][prev_col] > input_list[row][col]:
+            return True
+        return False
+
+
+def check_vertical(row_col_tuple):
+    row = row_col_tuple[0]
+    col = row_col_tuple[1]
+    if row == 0:
+        adj_down_row = row + 1
+        if input_list[adj_down_row][col] > input_list[row][col]:
+            return True
+        return False
+    elif row == len(input_list)-1:
+        adj_up_row = row - 1
+        if input_list[adj_up_row][col] > input_list[row][col]:
+            return True
+        return False
+    else:
+        adj_down_row = row + 1
+        adj_up_row = row - 1
+        if input_list[adj_up_row][col] > input_list[row][col] and input_list[adj_down_row][col] > input_list[row][col]:
+            return True
+        return False
+
+
+def risk_level(in_list):
+    risk = 0
+    for i in range(len(in_list)):
+        for k in range(len(in_list[i])):
+            row = i
+            col = k
+            vertical_check = check_vertical((row, col))
+            horizontal_check = check_horizontal((row, col))
+            if vertical_check and horizontal_check:
+                current = in_list[row][col]
+                risk += int(current) + 1
+                low_points.append((i, k))
+    return risk
+
+
+
+with open('input_test.txt') as input_file:
+    input_list = []
+    for line in input_file:
+        input_list.append(list(line.strip('\n')))
+risk = risk_level(input_list)
+print("Risk: ", risk)
+print("Low Points: ", low_points)
+for point in low_points:
+    check_basin(point)
+
+#so, i pass a cord to a method, it starts a set, then grabs all adjacent to that coord, adds to set if not 9.
+#then, for each item in set, check adjacent again and add any not 9's to the set, then go through the set again.
