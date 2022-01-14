9ad23bd6d046c7439bdbc00505dbc65b563cafe3
dillon <dhirth.it@gmail.com>
2022-01-14 01:13:12 -0600

    day11_part1, using classes now

--- day_11/day_11.py
+++ day_11/day_11.py
@@ -1,30 +1,58 @@
 # PART 1
 input_matrix = []
 
-def cascading_flash(row, col):
-    print("flash at:", row, ":", col)
-    input_matrix[row][col] = 0
+class indecent_flashing_octopus:
+    def __init__(self, age, row, col):
+        self.age = age
+        self.row = row
+        self.col = col
+        self.has_flashed = False
+        self.surrounding = [[row + 1, col - 1], [row + 1, col], [row + 1, col + 1],
+                       [row, col - 1], [row, col + 1],
+                       [row - 1, col - 1], [row - 1, col], [row - 1, col + 1]]
+    def __repr__(self):
+        return str(self.age)
 
-def increment(row, col):
-    print("before:", input_matrix[row][col])
-    input_matrix[row][col] = input_matrix[row][col] + 1
-    print("after:", input_matrix[row][col])
-    if input_matrix[row][col] > 9:
-        print("flash!, now go check others!")
-        cascading_flash(row, col)
+    def cascading_flash(self):
+        print("flash at:", row, ":", col)
+        for pair in self.surrounding:
+            input_matrix[pair[0]][pair[1]].increment()
+
+    def increment(self):
+        if self.has_flashed == False:
+            print("before:", self.age)
+            self.age += 1
+            print("after:", self.age)
+            if self.age > 9:
+                print("flash!, now go check others!")
+                self.age = 0
+                self.has_flashed = True
+                self.cascading_flash()
+        else:
+            pass
+
+
+
 
 with open('input_test2.txt') as input_file:
     for line in input_file:
         # convert string to list of integers then append to list of lists
         input_matrix.append(list(map(int, line.strip('\n'))))
+
+for row in range(len(input_matrix)):
+    for col in range(len(input_matrix[row])):
+        age = input_matrix[row][col]
+        input_matrix[row][col] = indecent_flashing_octopus(age, row, col)
+
 for step in range(2):
     for row in range(len(input_matrix)):
         for col in range(len(input_matrix[row])):
-            increment(row, col)
+            input_matrix[row][col].increment()
+
+
 
     with open('output.txt', 'a') as input_file:
         for line in input_matrix:
             # convert string to list of integers then append to list of lists
-            print(line)
             input_file.write(''.join(str(int) for int in line)+'\n')
         input_file.write('\n')
