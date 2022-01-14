3af280134757945958f816c5c1522c8b7178c290
dillon <dhirth.it@gmail.com>
2022-01-14 15:41:51 -0600

    day11_part1, seperating the alg steps out to be more defined.

--- day_11/day_11.py
+++ day_11/day_11.py
@@ -1,6 +1,7 @@
 # PART 1
 input_matrix = []
 
+
 class indecent_flashing_octopus:
     def __init__(self, age, row, col):
         self.age = age
@@ -8,33 +9,37 @@ class indecent_flashing_octopus:
         self.col = col
         self.has_flashed = False
         self.surrounding = [[row + 1, col - 1], [row + 1, col], [row + 1, col + 1],
-                       [row, col - 1], [row, col + 1],
-                       [row - 1, col - 1], [row - 1, col], [row - 1, col + 1]]
+                            [row, col - 1], [row, col + 1],
+                            [row - 1, col - 1], [row - 1, col], [row - 1, col + 1]]
+
     def __repr__(self):
         return str(self.age)
 
     def cascading_flash(self):
-        print("flash at:", row, ":", col)
-        for pair in self.surrounding:
-            input_matrix[pair[0]][pair[1]].increment()
+        if self.age > 9:
+            print("flash!")
+            self.has_flashed = True
+            for pair in self.surrounding:
+                try:
+                    input_matrix[pair[0]][pair[1]].increment()
+                except IndexError:
+                    pass
 
     def increment(self):
         if self.has_flashed == False:
             print("before:", self.age)
             self.age += 1
             print("after:", self.age)
-            if self.age > 9:
-                print("flash!, now go check others!")
-                self.age = 0
-                self.has_flashed = True
-                self.cascading_flash()
         else:
             pass
 
+    def reset_to_zero(self):
+        if self.has_flashed == True:
+            self.has_flashed = False
+            self.age = 0
 
 
-
-with open('input_test2.txt') as input_file:
+with open('input_test.txt') as input_file:
     for line in input_file:
         # convert string to list of integers then append to list of lists
         input_matrix.append(list(map(int, line.strip('\n'))))
@@ -48,11 +53,18 @@ for step in range(2):
     for row in range(len(input_matrix)):
         for col in range(len(input_matrix[row])):
             input_matrix[row][col].increment()
-
+    for row in range(len(input_matrix)):
+        for col in range(len(input_matrix[row])):
+            input_matrix[row][col].cascading_flash()
+    for row in range(len(input_matrix)):
+        for col in range(len(input_matrix[row])):
+            input_matrix[row][col].reset_to_zero()
 
 
     with open('output.txt', 'a') as input_file:
+        message = f"step: {step+1}"
+        input_file.write(message + '\n')
         for line in input_matrix:
             # convert string to list of integers then append to list of lists
-            input_file.write(''.join(str(int) for int in line)+'\n')
+            input_file.write(''.join(str(int) for int in line) + '\n')
         input_file.write('\n')
